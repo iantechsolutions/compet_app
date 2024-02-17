88fa4806f7abe3c5ece34bed0739a14994660b58
Tomás Cichero <martoxdlol@gmail.com>
2024-02-17 18:28:21 -0300

    service restart

--- src/scripts/lib/service.js
+++ src/scripts/lib/service.js
@@ -14,7 +14,9 @@ const serviceEntryPath = path.join(scriptPathname, '../../service-entrypoint.js'
 const svc = new Service({
     name: 'Compet MRP (actualización remota de datos)',
     description: 'Permite actualizar la base del MRP de forma remota.',
-    script: serviceEntryPath
+    script: serviceEntryPath,
+    maxRestarts: 1000,
+    wait: 10,
 });
 
 export { svc, serviceEntryPath };
