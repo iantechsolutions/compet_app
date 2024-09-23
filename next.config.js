ede5011208eb9d0e2b5c54c05fc642c4fc32d202
juli2kapo <info@iantech.com.ar>
2024-09-23 12:29:33 -0300

    added instrumentation hook true

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ await import('./src/env.js')
 
 /** @type {import("next").NextConfig} */
 const config = {
+    experimental:{
+        instrumentationHook: true
+    },
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
