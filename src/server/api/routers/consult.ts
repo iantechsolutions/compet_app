8c996261ab4665cacd7e8fbd85e3f880df4237aa
GD-gh <tomas9842@gmail.com>
2024-09-24 12:55:02 -0300

    consult productConsumo lista de duplas y yaConsumidoLoop

--- src/server/api/routers/consult.ts
+++ src/server/api/routers/consult.ts
@@ -22,7 +22,7 @@ export const consultRouter = createTRPCRouter({
             }))
         })
     )
-    .mutation(async ({ ctx, input }) => {
+    .mutation(async ({ input }) => {
         const data = await queryBaseMRPData();
         const session = await getServerAuthSession()
         const forecastProfileId = await getUserSetting<number>('mrp.current_forecast_profile', session?.user.id ?? "")
@@ -44,64 +44,93 @@ export const consultRouter = createTRPCRouter({
             !excludeProducts.some(excludedProduct => product.code.toLowerCase().startsWith(excludedProduct))
         )
         const eventsByProductCode = listProductsEvents(evolvedData, events)
-        const productConsumo = new Map<string,number>()
+        const productConsumo: [string, number][] = [];
+        const yaConsumidoLoop = new Map<string, number>();
+
         input.listado.forEach((product) => {
-            productConsumo.set(product.productCode, product.quantity)
+            productConsumo.push([product.productCode, product.quantity]);
         })
+
         let unDateable = false;
-        let buildDates: Date[] = [];
+        const arrivalDates = new Map<string, Date>();
+
         let i = 0;
-        while (i < productConsumo.size) {   
-            const product = curatedProducts.find((product) => product.code === Array.from(productConsumo.keys())[i])
+        while (i < productConsumo.length) { 
+            const pcKey = productConsumo[i]?.[0];
+            const pcValue = productConsumo[i]?.[1] ?? 0;
+
+            // no tiene sentido que esto llegue a cumplirse porque nunca sacamos elementos mientras iteramos
+            // pero el linter se queja asi que lo pongo igualmente
+            if (pcKey === undefined || pcValue === undefined) {
+                throw "Unexpected productConsumo[i] key/value undefined";
+            }
+
+            const product = curatedProducts.find((product) => product.code === pcKey);
             if (product) {
                 const expiredNotImportEvents = (eventsByProductCode.get(product.code) ?? []).filter(
                     (event) => event.expired && event.type !== 'import',
-                )
-                const commited = expiredNotImportEvents.reduce((sum, event) => sum + event.quantity, 0)
-                if((productConsumo.get(product.code) ?? 0) > product.stock - commited) {
-                    if(product.supplies && product.supplies.length > 0 ){
+                );
+
+                const commited = expiredNotImportEvents.reduce((sum, event) => sum + event.quantity, 0);
+                const consumedTotal = commited + (yaConsumidoLoop.get(pcKey) ?? 0);
+
+                // falta
+                if (pcValue > product.stock - consumedTotal) {
+                    if (product.supplies && product.supplies.length > 0 ){
                         // productConsumo.set(product.code, 0)
-                        product.supplies.forEach(supply=>{
-                            productConsumo.set(supply.supply_product_code, ((productConsumo.get(supply.product_code) ?? 0) + (supply.quantity * ((productConsumo.get(product.code) ?? 0) 
-                            // - (product.stock - commited )  //?????????????? revisar
-                        ))))
-                        })                        
-                    }
-                    else{
+                        product.supplies.forEach(supply => {
+                            const productSupply = productConsumo.find((v) => supply.product_code === v[0])?.[1] ?? 0;
+                            productConsumo.push([supply.supply_product_code, (productSupply + (supply.quantity * (pcValue 
+                                // - (product.stock - commited o consumedTotal)  //?????????????? revisar
+                            )))])
+                        });
+
+                        // no se consume porque no hay, solo se agregan los supplies los cuales van a consumirse en prox iter.
+                        // yaConsumidoLoop.set(pcKey, (yaConsumidoLoop.get(pcKey) ?? 0) + pcValue);
+                    } else {
                         let validAmount = false;
-                        product.imports.filter(impor=>new Date(String(impor.arrival_date)).getTime() > new Date().getTime())
-                        //AGREGAR SORT?????, PARA QUE EMPIEZE DESDE IMPORT MAS CERCANO
-                        .forEach(impor=>{
-                            if((productConsumo.get(product.code) ?? 0) < (product.stock - commited + impor.ordered_quantity) && !validAmount ){
-                                const tempDate = new Date(String(impor.arrival_date))
-                                buildDates.push(tempDate);
-                                validAmount = true;
-                            }
-                        })
-                        if(!validAmount){
+                        // AGREGAR SORT?????, PARA QUE EMPIEZE DESDE IMPORT MAS CERCANO
+                        // product.imports.sort((a, b) => a.arrival_date.getTime() - b.arrival_date.getTime());
+                        product.imports.filter(impor => new Date(String(impor.arrival_date)).getTime() > new Date().getTime())
+                            .forEach(impor=>{
+                                if(pcValue < (product.stock - consumedTotal + impor.ordered_quantity) && !validAmount ){
+                                    arrivalDates.set(product.code, new Date(String(impor.arrival_date)));
+                                    validAmount = true;
+                                }
+                            });
+
+                        if (!validAmount){
                             unDateable = true;
+                        } else {
+                            // se consume incluyendo el import
+                            yaConsumidoLoop.set(pcKey, (yaConsumidoLoop.get(pcKey) ?? 0) + pcValue);
                         }
                     }
+                } else { // alcanza y se consume
+                    yaConsumidoLoop.set(pcKey, (yaConsumidoLoop.get(pcKey) ?? 0) + pcValue);
                 }
             }
             i++;
         }
-        if (unDateable){
-            return {
-                isPossible: false,
-                buildDate: null 
-            }
-        }
-        else if(buildDates.length > 0){
-            return {
-                isPossible: false,
-                buildDate: Math.max(...buildDates.map(date => date.getTime()))
-            }
-        }
-        return{
-                isPossible: true,
-            }
 
-        
+        const objRes: {
+            isPossible: boolean,
+            buildDate: null | number,
+            arrivalDates: Map<string, Date>,
+        } = {
+            isPossible: false,
+            buildDate: null, // debería llamarse arrivalDate pero por cuestiones de frontend por las dudas no lo toco
+            arrivalDates,
+        };
+
+        if (unDateable){
+            // intacto
+        } else if (arrivalDates.size > 0){
+            objRes.buildDate = Math.max(...[...arrivalDates.values()].map(date => date.getTime()));
+        } else {
+            objRes.isPossible = true;
+        }
+
+        return objRes;
     })
 })
