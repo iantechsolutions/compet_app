44cd497ba1eb28ad00ee12747ee8c03c8d978aa5
GD-gh <tomas9842@gmail.com>
2024-10-03 12:36:58 -0300

    cache fetch with mutex

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,28 +1,24 @@
-type CacheEntry = {expiresAt: number, value: unknown};
+import {Mutex} from 'async-mutex';
+
+type CacheEntry = {expiresAt: number, fetchMutex: Mutex, value: unknown};
 const Cache: Record<string, CacheEntry> = {};
-
-export function cachedSyncFetch<T>(key: string, ttlMs: number, fetchCallback: () => T): T {
-    if (typeof Cache[key]?.expiresAt === 'number' && Cache[key].expiresAt > Date.now()) {
-        return Cache[key].value as T;
-    }
-
-    Cache[key] = {
-        expiresAt: Date.now() + ttlMs,
-        value: fetchCallback()
-    };
-
-    return Cache[key].value as T;
-}
 
 export async function cachedAsyncFetch<T>(key: string, ttlMs: number, fetchCallback: () => Promise<T>): Promise<T> {
     if (typeof Cache[key]?.expiresAt === 'number' && Cache[key].expiresAt > Date.now()) {
         return Cache[key].value as T;
+    } else if (typeof Cache[key]?.expiresAt === 'number') {
+        await Cache[key].fetchMutex.runExclusive(async () => {
+            if (Cache[key] !== undefined) {
+                Cache[key].value = await fetchCallback();
+            }
+        });
+    } else {
+        Cache[key] = {
+            expiresAt: Date.now() + ttlMs,
+            value: await fetchCallback(),
+            fetchMutex: new Mutex(),
+        };
     }
-
-    Cache[key] = {
-        expiresAt: Date.now() + ttlMs,
-        value: await fetchCallback()
-    };
 
     return Cache[key].value as T;
 }
